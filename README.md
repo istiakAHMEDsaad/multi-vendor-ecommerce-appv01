4c4eb8f136f96864e298b76f79921291c36d450e
istiakAHMEDsaad <istik2020@gmail.com>
2025-12-22 15:28:33 +0600

    store post data api complete!

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 [NeonsDB Docs](https://neon.com/docs/guides/prisma)
 [Clerk Docs](https://clerk.com/docs/nextjs/guides/development/custom-sign-up-page)
 [Inngest Docs](https://www.inngest.com/docs/getting-started/nextjs-quick-start)
+[ImageKit Docs](https://www.npmjs.com/package/imagekit#file-upload)
--- app/api/store/create/route.js
+++ app/api/store/create/route.js
@@ -1,3 +1,4 @@
+import imagekit from '@/configs/imageKit';
 import prisma from '@/lib/prisma';
 import { getAuth } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
@@ -45,12 +46,53 @@ export async function POST(request) {
     const isUserNameTaken = await prisma.store.findFirst({
       where: { username: username.toLowerCase() },
     });
-
     if (isUserNameTaken) {
       return NextResponse.json(
         { error: 'Username already taken' },
         { status: 400 }
       );
     }
-  } catch (error) {}
+
+    // image upload to imagekit
+    const buffer = Buffer.from(await image.arrayBuffer());
+    const response = await imagekit.upload({
+      file: buffer,
+      fileName: image.name,
+      folder: 'logos',
+    });
+
+    const optimizedImage = imagekit.url({
+      path: response.filePath,
+      transformation: [
+        { quality: 'auto' },
+        { format: 'webp' },
+        { width: '512' },
+      ],
+    });
+
+    const newStore = await prisma.store.create({
+      data: {
+        userId,
+        name,
+        description,
+        username: username.toLowerCase(),
+        email,
+        contact,
+        address,
+        logo: optimizedImage,
+      },
+    });
+
+    // Link store to user
+    await prisma.user.update({
+      where: { id: userId },
+      data: { store: { connect: { id: newStore.id } } },
+    });
+
+    return NextResponse.json({ message: 'Applied, wating for approval' });
+  } catch (error) {
+    console.error(error.error);
+    NextResponse.json({ error: error.code || error.message }, { status: 400 });
+  }
 }
+
