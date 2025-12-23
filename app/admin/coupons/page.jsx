7240dfe58e3eef043ed85223c59e7d39e437a6b6
istiakAHMEDsaad <istik2020@gmail.com>
2025-12-23 21:20:31 +0600

    free coupon added

--- app/admin/coupons/page.jsx
+++ app/admin/coupons/page.jsx
@@ -3,9 +3,13 @@ import { useEffect, useState } from 'react';
 import { format } from 'date-fns';
 import toast from 'react-hot-toast';
 import { DeleteIcon } from 'lucide-react';
-import { couponDummyData } from '@/assets/assets';
+// import { couponDummyData } from '@/assets/assets';
+import { useAuth } from '@clerk/nextjs';
+import axios from 'axios';
 
 export default function AdminCoupons() {
+  const { getToken } = useAuth();
+
   const [coupons, setCoupons] = useState([]);
 
   const [newCoupon, setNewCoupon] = useState({
@@ -19,12 +23,40 @@ export default function AdminCoupons() {
   });
 
   const fetchCoupons = async () => {
-    setCoupons(couponDummyData);
+    try {
+      const token = await getToken();
+      const { data } = await axios.get('/api/admin/coupon', {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      setCoupons(data.coupons);
+    } catch (error) {
+      toast.error(error?.response?.data?.error || error.message);
+    }
   };
 
   const handleAddCoupon = async (e) => {
     e.preventDefault();
     // Logic to add a coupon
+
+    try {
+      const token = await getToken();
+
+      newCoupon.discount = Number(newCoupon.discount);
+      newCoupon.expiresAt = new Date(newCoupon.expiresAt);
+
+      const { data } = await axios.post(
+        '/api/admin/coupon',
+        { coupon: newCoupon },
+        { headers: { Authorization: `Bearer ${token}` } }
+      );
+
+      toast.success(data.message);
+
+      await fetchCoupons();
+    } catch (error) {
+      toast.error(error?.response?.data?.error || error.message);
+    }
   };
 
   const handleChange = (e) => {
@@ -33,6 +65,23 @@ export default function AdminCoupons() {
 
   const deleteCoupon = async (code) => {
     // Logic to delete a coupon
+    try {
+      const confirm = window.confirm(
+        'Are you sure you want to delete this coupon?'
+      );
+      if (!confirm) return;
+
+      const token = await getToken();
+
+      await axios.delete(`/api/admin/coupon?code=${code}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      await fetchCoupons();
+      toast.success('Coupon deleted successfully');
+    } catch (error) {
+      toast.error(error?.response?.data?.error || error.message);
+    }
   };
 
   useEffect(() => {
@@ -107,7 +156,7 @@ export default function AdminCoupons() {
                   setNewCoupon({ ...newCoupon, forNewUser: e.target.checked })
                 }
               />
-              <div className='w-11 h-6 bg-slate-300 rounded-full peer peer-checked:bg-green-600 transition-colors duration-200'></div>
+              <div className='w-11 h-6 bg-slate-300 rounded-full peer peer-checked:bg-amber-600 transition-colors duration-200'></div>
               <span className='dot absolute left-1 top-1 w-4 h-4 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-5'></span>
             </label>
             <p>For New User</p>
@@ -123,7 +172,7 @@ export default function AdminCoupons() {
                   setNewCoupon({ ...newCoupon, forMember: e.target.checked })
                 }
               />
-              <div className='w-11 h-6 bg-slate-300 rounded-full peer peer-checked:bg-green-600 transition-colors duration-200'></div>
+              <div className='w-11 h-6 bg-slate-300 rounded-full peer peer-checked:bg-amber-600 transition-colors duration-200'></div>
               <span className='dot absolute left-1 top-1 w-4 h-4 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-5'></span>
             </label>
             <p>For Member</p>
