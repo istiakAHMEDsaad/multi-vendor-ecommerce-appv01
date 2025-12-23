c1ed1e27cb3d2b5bb27780324cab5f88207da8c5
istiakAHMEDsaad <istik2020@gmail.com>
2025-12-23 15:09:33 +0600

    bug fixed logout

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,4 +1,5 @@
 import AdminLayout from '@/components/admin/AdminLayout';
+import { SignedIn, SignedOut, SignIn } from '@clerk/nextjs';
 
 export const metadata = {
   title: 'GoCart. - Admin',
@@ -8,7 +9,15 @@ export const metadata = {
 export default function RootAdminLayout({ children }) {
   return (
     <>
-      <AdminLayout>{children}</AdminLayout>
+      <SignedIn>
+        <AdminLayout>{children}</AdminLayout>
+      </SignedIn>
+
+      <SignedOut>
+        <div className='min-h-screen flex items-center justify-center'>
+          <SignIn fallbackRedirectUrl='/admin' routing='hash' />
+        </div>
+      </SignedOut>
     </>
   );
 }
