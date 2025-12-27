d1587d9eb65fc2bf3069dc6ebdc4f80c826b92dd
istiakAHMEDsaad <istik2020@gmail.com>
2025-12-27 13:26:28 +0600

    added stripe payment method 💻

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-[NeonsDB Docs](https://neon.com/docs/guides/prisma)
-[Clerk Docs](https://clerk.com/docs/nextjs/guides/development/custom-sign-up-page)
-[Inngest Docs](https://www.inngest.com/docs/getting-started/nextjs-quick-start)
-[ImageKit Docs](https://www.npmjs.com/package/imagekit#file-upload)
+[Live Link](https://multi-vendor-ecommerce-appv01.vercel.app/)
+
+[NeonsDB Docs](https://neon.com/docs/guides/prisma)<br/>
+[Clerk Docs](https://clerk.com/docs/nextjs/guides/development/custom-sign-up-page)<br/>
+[Inngest Docs](https://www.inngest.com/docs/getting-started/nextjs-quick-start)<br/>
+[ImageKit Docs](https://www.npmjs.com/package/imagekit#file-upload)<br/>
+[Stripe Session Docs](https://docs.stripe.com/api/checkout/sessions/create?lang=node)<br/>
--- app/api/orders/route.js
+++ app/api/orders/route.js
@@ -2,6 +2,7 @@ import prisma from '@/lib/prisma';
 import { getAuth } from '@clerk/nextjs/server';
 import { PaymentMethod } from '@prisma/client';
 import { NextResponse } from 'next/server';
+import Stripe from 'stripe';
 
 // cash on delivery methods
 export async function POST(request) {
@@ -120,6 +121,39 @@ export async function POST(request) {
       });
 
       orderIds.push(order.id);
+    }
+
+    if (paymentMethod === 'STRIPE') {
+      const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+      const origin = await request.headers.get('origin');
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [
+          {
+            price_data: {
+              currency: 'usd',
+              product_data: {
+                name: 'Order',
+              },
+              unit_amount: Math.round(fullAmount * 100),
+            },
+            quantity: 1,
+          },
+        ],
+        // current time + 30 min
+        expires_at: Math.floor(Date.now() / 1000) + 30 * 60,
+        mode: 'payment',
+        success_url: `${origin}/loading?nextUrl=orders`,
+        cancel_url: `${origin}/cart`,
+        metadata: {
+          orderIds: orderIds.join(','),
+          userId,
+          appId: 'ninecart',
+        },
+      });
+
+      return NextResponse.json({session})
     }
 
     // clear the cart:
